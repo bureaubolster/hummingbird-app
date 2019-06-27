34c29007112b02b9af3afa4ca92fc23be15e97bd
Mark Jackson <mark.j.jackson@me.com>
2019-06-27 11:20:27 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const {
+  getDesign,
+  getTypographyConfig,
+  getFooterConfig
+} = require('./node/utils');
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: '',
+    siteUrl: process.env.SITE_URL || process.env.URL || 'https://wings.dev'
+  },
+  __experimentalThemes: [
+    {
+      resolve: '@wingscms/hummingbird',
+      options: {
+        wings: {
+          project: process.env.GATSBY_WINGS_PROJECT,
+          appKey: process.env.GATSBY_WINGS_APP_KEY,
+          endpoint: process.env.GATSBY_WINGS_ENDPOINT
+        },
+        blockRobots:
+          process.env.BLOCK_ROBOTS || process.env.GATSBY_ENV !== 'production',
+        gaTrackingId: process.env.GA_TRACKING_ID,
+        basicAuth: process.env.BASIC_AUTH,
+        campaigns: {},
+        design: {
+          ...getDesign()
+        },
+        typography: {
+          ...getTypographyConfig()
+        },
+        footer: {
+          title: '',
+          logoUrl: '',
+          logoLink: '',
+          columns: [],
+          ...getFooterConfig()
+        }
+      }
+    }
+  ]
+};
